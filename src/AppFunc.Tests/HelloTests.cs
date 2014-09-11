fcbb2d4e71693590a9c7a2f4682eceaa251af29c
Craig Smitham <craigsmitham@gmail.com>
2014-09-10 19:14:35 -0500

    moving tests project

---
+++ src/AppFunc.Tests/HelloTests.cs
@@ -0,0 +1,14 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace AppFunc.Tests
+{
+    public class HelloTests
+    {
+        [Fact]
+        public void ShouldAlwaysPass()
+        {
+            true.ShouldBe(true);
+        }
+    }
+}
