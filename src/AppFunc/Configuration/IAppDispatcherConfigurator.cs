985c2d4572c1dcff9ec1a08680ae7451c35a6de6
Craig Smitham <craigsmitham@gmail.com>
2014-09-21 21:36:38 -0500

    More comments!

--- src/AppFunc/Configuration/IAppDispatcherConfigurator.cs
+++ src/AppFunc/Configuration/IAppDispatcherConfigurator.cs
@@ -5,23 +5,60 @@ namespace AppFunc.Configuration
 {
     public interface IAppDispatcherConfigurator
     {
+        /// <summary>
+        /// Use the specified dependecy resolver. Install NuGet package AppFunc.CommonServiceLocator 
+        /// to use the CommonServiceLocatorDependencyResolver, or implement your own.
+        /// </summary>
+        /// <param name="dependencyResolver"></param>
         void UseDependencyResolver(IDependencyResolver dependencyResolver);
 
+        /// <summary>
+        /// Configure the global pipeline. Ensure the inner action is invoked all the way down, otherwise
+        /// the request dispatcher will fail.
+        /// </summary>
+        /// <param name="pipeline">The pipeline configuration function, the Action paremeter represents the next pipeline action to be invoked.</param>
+        /// <param name="supressValidation">Suppresses an initial firing of the pipeline that validates it is configured correctly.</param>
         void GlobalPipeline(Func<Action, Action> pipeline, bool supressValidation = false);
 
 
+        /// <summary>
+        /// Registers a void request handler
+        /// </summary>
+        /// <typeparam name="TRequest"></typeparam>
+        /// <param name="handler"></param>
+        /// <param name="handlerConfig"></param>
         void RegisterHandler<TRequest>(Action<TRequest> handler,
             Func<Action<TRequest>, Action<TRequest>> handlerConfig = null)
             where TRequest : IRequest;
 
+        /// <summary>
+        /// Registers a request/response handler
+        /// </summary>
+        /// <typeparam name="TRequest"></typeparam>
+        /// <typeparam name="TResponse"></typeparam>
+        /// <param name="handler"></param>
+        /// <param name="handlerConfig"></param>
         void RegisterHandler<TRequest, TResponse>(Func<TRequest, TResponse> handler,
             Func<Func<TRequest, TResponse>, Func<TRequest, TResponse>> handlerConfig = null)
             where TRequest : IRequest<TResponse>;
 
+        /// <summary>
+        /// Registers an async request handler
+        /// </summary>
+        /// <typeparam name="TRequest"></typeparam>
+        /// <param name="handler"></param>
+        /// <param name="handlerConfig"></param>
         void RegisterHandler<TRequest>(Func<TRequest, Task> handler,
             Func<Func<TRequest, Task>, Func<TRequest, Task>> handlerConfig = null)
             where TRequest : IAsyncRequest;
 
+        /// <summary>
+        /// Registers an async request/response handler
+        /// </summary>
+        /// <typeparam name="TRequest"></typeparam>
+        /// <typeparam name="TResponse"></typeparam>
+        /// <param name="handler"></param>
+        /// <param name="handlerConfig"></param>
         void RegisterHandler<TRequest, TResponse>(Func<TRequest, Task<TResponse>> handler,
             Func<Func<TRequest, Task<TResponse>>, Func<TRequest, Task<TResponse>>> handlerConfig = null)
             where TRequest : IAsyncRequest<TResponse>;
--- src/AppFunc/Configuration/LambdaDecoratorHelpers.cs
+++ src/AppFunc/Configuration/LambdaDecoratorHelpers.cs
@@ -4,22 +4,44 @@ namespace AppFunc.Configuration
 {
     public static class LambdaDecoratorHelpers
     {
-        public static Func<TRequest, TResponse> Decorate<TRequest, TResponse>(this Func<TRequest, TResponse> handler,
+        /// <summary>
+        /// Decorates a request/response handler function
+        /// </summary>
+        /// <typeparam name="TRequest"></typeparam>
+        /// <typeparam name="TResponse"></typeparam>
+        /// <param name="inner"></param>
+        /// <param name="decorator"></param>
+        /// <returns></returns>
+        public static Func<TRequest, TResponse> Decorate<TRequest, TResponse>(this Func<TRequest, TResponse> inner,
             Func<TRequest, Func<TRequest, TResponse>, TResponse> decorator)
         {
-            return request => decorator(request, handler);
+            return request => decorator(request, inner);
         }
 
-        public static Action<TRequest> Decorate<TRequest>(this Action<TRequest> handler,
-            Action<TRequest, Action<TRequest>> decorator)
+
+        /// <summary>
+        /// Decorates a request handler function
+        /// </summary>
+        /// <typeparam name="TRequest"></typeparam>
+        /// <param name="decorated"></param>
+        /// <param name="inner"></param>
+        /// <returns></returns>
+        public static Action<TRequest> Decorate<TRequest>(this Action<TRequest> decorated,
+            Action<TRequest, Action<TRequest>> inner)
         {
-            return request => decorator(request, handler);
+            return request => inner(request, decorated);
         }
 
-        public static Action Decorate(this Action handler,
+        /// <summary>
+        /// Decorates an inner action
+        /// </summary>
+        /// <param name="decorated"></param>
+        /// <param name="decorator"></param>
+        /// <returns></returns>
+        public static Action Decorate(this Action decorated,
             Action<Action> decorator)
         {
-            return () => decorator(handler);
+            return () => decorator(decorated);
         }
     }
 }
